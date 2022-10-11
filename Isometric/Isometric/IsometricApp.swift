b272aa288c3c1b82aea109140715744ea8dd4474
kiyong <kdo8106@metlife.co.kr>
2022-10-11 14:46:44 +0900

    add Isometric

---
+++ Isometric/Isometric/IsometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricApp.swift
+//  Isometric
+//
+//  Created by Do Kiyong on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct IsometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
