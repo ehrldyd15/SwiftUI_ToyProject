2d8d8e40d8b6974f48b0bc7afa981f257bf18357
kiyong <kdo8106@metlife.co.kr>
2022-10-11 11:42:04 +0900

    티스트커밋ㅎ

--- SwiftUI_ToyProject/ContentView.swift
+++ SwiftUI_ToyProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world!으하하하")
         }
         .padding()
     }
