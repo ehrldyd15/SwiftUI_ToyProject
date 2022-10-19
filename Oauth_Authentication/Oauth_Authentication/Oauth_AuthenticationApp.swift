011f4e3a3b692013b795995ad4ae301e90f1f130
kiyong <dky0979@gmail.com>
2022-10-19 11:04:16 +0900

    Oath_Authentication 프로젝트 추가

---
+++ Oauth_Authentication/Oauth_Authentication/Oauth_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oauth_AuthenticationApp.swift
+//  Oauth_Authentication
+//
+//  Created by Do Kiyong on 2022/10/19.
+//
+
+import SwiftUI
+
+@main
+struct Oauth_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
