c7134d26e0eade8626ee8b7b10386b4f240a78ab
kiyong <dky0979@gmail.com>
2022-10-25 10:21:13 +0900

    [Book Animation] 프로젝트 세팅

---
+++ BookAnimation/BookAnimation/BookAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAnimationApp.swift
+//  BookAnimation
+//
+//  Created by Do Kiyong on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BookAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
