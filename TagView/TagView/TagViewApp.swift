1fd2ffd1ad31507e12d825f73cc47431ffe47985
kiyong <dky0979@gmail.com>
2022-11-11 16:31:35 +0900

    [TagView] 프로젝트 세팅

---
+++ TagView/TagView/TagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagViewApp.swift
+//  TagView
+//
+//  Created by Do Kiyong on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct TagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
