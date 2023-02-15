9ce7f07ebabadde22cf73432800d3fbd077b36be
kiyong <dky0979@gmail.com>
2023-02-15 10:00:21 +0900

    Marvel API 추가

---
+++ MiniProject/MarvelAPI/MarvelAPI/MarvelAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAPIApp.swift
+//  MarvelAPI
+//
+//  Created by Do Kiyong on 2023/02/15.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
