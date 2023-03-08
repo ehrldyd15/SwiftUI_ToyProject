e402d811acf7f7527498f623118afc1821c49b47
kiyong <dky0979@gmail.com>
2023-03-08 15:16:53 +0900

    [Weather]

--- MiniProject/Weather/Weather/Resources/Views/Main/HomeView.swift
+++ MiniProject/Weather/Weather/Resources/Views/Main/HomeView.swift
@@ -15,18 +15,24 @@ enum BottomSheetPosition: CGFloat, CaseIterable {
 
 struct HomeView: View {
     @State var bottomSheetPosition: BottomSheetPosition = .middle
+    @State var bottomSheetTranslation: CGFloat = BottomSheetPosition.middle.rawValue
+    @State var hasDragged: Bool = false
+    
+    var bottomSheetTranslationProrated: CGFloat {
+        (bottomSheetTranslation - BottomSheetPosition.middle.rawValue) / (BottomSheetPosition.top.rawValue - BottomSheetPosition.middle.rawValue)
+    }
     
     private var attributedString: AttributedString {
-        var string = AttributedString("19°" + "\n" + "Mostly Clear")
+        var string = AttributedString("19°" + (hasDragged ? " | " : "\n ") + "Mostly Clear")
         
         if let temp = string.range(of: "19°") {
-            string[temp].font = .system(size: 96, weight: .thin)
-            string[temp].foregroundColor = .primary
+            string[temp].font = .system(size: (96 - (bottomSheetTranslationProrated * (96 - 20))), weight: hasDragged ? .semibold : .thin)
+            string[temp].foregroundColor = hasDragged ? .secondary : .primary
         }
 
         if let pipe = string.range(of: " | ") {
             string[pipe].font = .title3.weight(.semibold)
-            string[pipe].foregroundColor = .secondary
+            string[pipe].foregroundColor = .secondary.opacity(bottomSheetTranslationProrated)
         }
         
         if let weather = string.range(of: "Mostly Clear") {
@@ -39,45 +45,71 @@ struct HomeView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                // MARK: Background Color
-                Color.background
-                    .ignoresSafeArea()
+            GeometryReader { geometry in
+                let screenHeight = geometry.size.height + geometry.safeAreaInsets.top + geometry.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
                 
-                // MARK: Background Image
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                Image("House")
-                    .frame(maxWidth: .infinity, alignment: .top)
-                    .padding(.top, 257)
-                
-                VStack(spacing: -10) {
-                    Text("Montreal")
-                        .font(.largeTitle)
+                ZStack {
+                    // MARK: Background Color
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack {
-                        Text(attributedString)
+                    // MARK: Background Image
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    // MARK: House Image
+                    Image("House")
+                        .frame(maxWidth: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationProrated * imageOffset)
+                    
+                    // MARK: Current Weather
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationProrated)) {
+                        Text("Montreal")
+                            .font(.largeTitle)
                         
-                        Text("H:24°   L:18")
-                            .font(.title3.weight(.semibold))
+                        VStack {
+                            Text(attributedString)
+                            
+                            Text("H:24°   L:18")
+                                .font(.title3.weight(.semibold))
+                                .opacity(1 - bottomSheetTranslationProrated)
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(51)
+                    .offset(y: -bottomSheetTranslationProrated * 46)
+                    
+                    // MARK: Bottom Sheet
+                    BottomSheetView(position: $bottomSheetPosition) {
+                        Text(bottomSheetTranslationProrated.formatted())
+                    } content: {
+                        ForecastView(bottomSheetTranslationProrated: bottomSheetTranslationProrated)
+                    }
+                    .onBottomSheetDrag { translation in
+                        // translation은 스크린 높이이기 때문에
+                        // BottomSheetPosition의 rawValue로 변환하려면 safeArea를 포함한 스크린 높이를 나눠준다.
+                        bottomSheetTranslation = translation / screenHeight
+                        
+                        withAnimation(.easeInOut) {
+                            if bottomSheetPosition == BottomSheetPosition.top {
+                                hasDragged = true
+                            } else {
+                                hasDragged = false
+                            }
+                        }
                     }
                     
-                    Spacer()
+                    // MARK: Tab Bar
+                    TabBar(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationProrated * 115)
                 }
-                .padding(51)
-                
-                // MARK: Bottom Sheet
-                BottomSheetView(position: $bottomSheetPosition) {
-//                    Text(bottomSheetPosition.rawValue.formatted())
-                } content: {
-                    ForecastView()
-                }
-                
-                TabBar(action: {
-                    bottomSheetPosition = .top
-                })
             }
             .navigationBarHidden(true)
         }
