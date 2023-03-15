50bc4e946b5b4f443bb70c8c3a177c65d4ce9252
kiyong <dky0979@gmail.com>
2023-03-15 13:58:24 +0900

    [Weather]

---
+++ MiniProject/Weather/Weather/Resources/Views/Detail/WeatherView.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Do Kiyong on 2023/03/15.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        ZStack {
+            // MARK: Background
+            Color.background
+                .ignoresSafeArea()
+        }
+        .overlay {
+            // MARK: Navigation Bar
+            NavigationBar()
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+            .preferredColorScheme(.dark)
+    }
+}
--- MiniProject/Weather/Weather/Resources/Views/Navigation/TabBar.swift
+++ MiniProject/Weather/Weather/Resources/Views/Navigation/TabBar.swift
@@ -35,7 +35,7 @@ struct TabBar: View {
                 
                 // MARK: Navigation Button
                 NavigationLink {
-                    
+                    WeatherView()
                 } label: {
                     Image(systemName: "list.star")
                         .frame(width: 44, height: 44)
