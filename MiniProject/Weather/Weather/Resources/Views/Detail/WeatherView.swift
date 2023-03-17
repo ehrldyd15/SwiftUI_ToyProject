883ba05a667cd086182a15c160297a3dc7beb034
kiyong <dky0979@gmail.com>
2023-03-17 15:27:39 +0900

    [Weather] 완료

--- MiniProject/Weather/Weather/Resources/Views/Detail/WeatherView.swift
+++ MiniProject/Weather/Weather/Resources/Views/Detail/WeatherView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct WeatherView: View {
+    @State private var searchText = ""
+    
+    var searchResults: [Forecast] {
+        if searchText.isEmpty {
+            return Forecast.cities
+        } else {
+            return Forecast.cities.filter { $0.location.contains(searchText) }
+        }
+    }
+    
     var body: some View {
         ZStack {
             // MARK: Background
             Color.background
                 .ignoresSafeArea()
+            
+            // MARK: Weather Widgets
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 20) {
+                    ForEach(searchResults) { forecast in
+                        WeatherWidget(forecast: forecast)
+                    }
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                EmptyView()
+                    .frame(height: 110)
+            }
         }
         .overlay {
             // MARK: Navigation Bar
-            NavigationBar()
+            NavigationBar(searchText: $searchText)
         }
         .navigationBarHidden(true)
+//        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Search for a city or airport")
     }
 }
 
