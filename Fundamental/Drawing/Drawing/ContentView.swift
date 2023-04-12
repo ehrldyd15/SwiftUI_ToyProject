2b3cd48428349203c602c9e66eebd56f1535ee04
kiyong <dky0979@gmail.com>
2023-04-12 14:52:50 +0900

    no message

--- Fundamental/Drawing/Drawing/ContentView.swift
+++ Fundamental/Drawing/Drawing/ContentView.swift
@@ -12,10 +12,13 @@ import SwiftUI
 // https://www.youtube.com/watch?v=HkSoE0j8u90
 // https://www.youtube.com/watch?v=sQ89JRq0kvg
 // https://www.youtube.com/watch?v=23rvgRZVvLM
+// https://www.youtube.com/watch?v=OFf5ZEd3Yzw
 
 struct ContentView: View {
     @State private var petalOffset = -20.0
     @State private var petalWidth = 100.0
+    
+    @State private var colorCycle = 0.0
     
     var body: some View {
         TabView {
@@ -105,15 +108,30 @@ struct ContentView: View {
             }
             .tabItem {
                 Image(systemName: "3.square.fill")
-                Text("First")
+                Text("Flower")
             }
             
 //            Text("path/Shape")
-//                .tabItem {
-//                    Image(systemName: "3.square.fill")
-//                    Text("First")
-//                }
+//                .frame(width: 300, height: 300)
+//                .border(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.4, width: 1, height: 0.1), scale: 0.2), width: 50)
+            Capsule()
+                .strokeBorder(ImagePaint(image: Image("Example"), sourceRect: CGRect(x: 0, y: 0.25, width: 1, height: 0.5), scale: 0.3), lineWidth: 20)
+                .frame(width: 300, height: 200)
+                .tabItem {
+                    Image(systemName: "4.square.fill")
+                    Text("ImagePaint")
+                }
             
+            VStack {
+                ColorCyclingCircle(amount: colorCycle)
+                    .frame(width: 300, height: 300)
+                
+                Slider(value: $colorCycle)
+            }
+            .tabItem {
+                Image(systemName: "5.square.fill")
+                Text("MetalRendering")
+            }
         }
     }
 }
@@ -121,6 +139,46 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        // 많은 수의 gradient, shadow 및 기타 효과를 사용하다보면 성능 문제가 발생할 가능성이 크기 때문에 drawingGroup()를 사용하자
+        // drawingGroup()은 최종 표시 전에 뷰의 내용을 offscreen 이미지로 결합한다.
+        // offscreen 구성은 애플의 고성능 그래픽 프레임워크인 Metal을 사용하여 복잡한 View를 렌더링하는데 있어 인상적으로 속도를 향상시킨다.
+        // view의 내용은 단일 비트맵으로 구성되며, 비트맵은 뷰 대신 표현된다.
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
     }
 }
 
