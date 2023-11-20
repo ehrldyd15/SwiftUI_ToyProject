54857b6431e1d7bf921e2e2bc18e91b8e7762187
kiyong <dky0979@gmail.com>
2023-11-20 10:02:55 +0900

    no message

---
+++ MiniProject/CoordinatorTest/CoordinatorTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoordinatorTest
+//
+//  Created by Do Kiyong on 11/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniProject/CoordinatorTest/CoordinatorTest/CoordinatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorTestApp.swift
+//  CoordinatorTest
+//
+//  Created by Do Kiyong on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
