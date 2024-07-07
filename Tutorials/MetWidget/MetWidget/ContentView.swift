8f198a51163348fea581d3e410f83e5809c43f3d
kiyong <dky0979@gmail.com>
2024-07-08 08:45:22 +0900

    위젯 프로젝트 추가

---
+++ Tutorials/MetWidget/MetWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetWidget
+//
+//  Created by Do Kiyong on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tutorials/MetWidget/MetWidget/MetWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetWidgetApp.swift
+//  MetWidget
+//
+//  Created by Do Kiyong on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MetWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
