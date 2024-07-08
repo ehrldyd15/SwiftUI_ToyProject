401a992346694f1d50b1725afc7d51ea9feda85e
kiyong <dky0979@gmail.com>
2024-07-08 09:16:46 +0900

    Composable Architecture 버전 업데이트

---
+++ Tutorials/MetWidget/CertWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  CertWidget
+//
+//  Created by Do Kiyong on 7/8/24.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
